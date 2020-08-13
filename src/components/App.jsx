bafbdfa1b79eb39ee224c63d492c7adcdef4ae05
Newton <kamaunewton78@gmail.com>
2020-08-13 10:10:12 +0300

    used map()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,21 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avator from "./Avator";
 
+function createContact(contact) {
+    return <Card
+        key={contact.id}
+        name={contact.name}
+        img={contact.imgURL}
+        tel={contact.phone}
+        email={contact.email}
+    />;
+}
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avator img="https://cdn.shopify.com/s/files/1/0161/1156/articles/IMG_3780_medium.jpg?v=1479130925" />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createContact)}
     </div>
   );
 }
